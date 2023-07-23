c18813ff0337d44fd13c3b27af0e13aded8a4eaa
duyhieu108 <duyhieu1008@gmail.com>
2023-07-23 23:37:09 +0800

    update

---
+++ .circleci/src/index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = require('url');
+var server = http.createServer(function (req, res) {
+        'use strict';
+        res.writeHead(200);
+        var q = url.parse(req.url, true).query;
+        if (q.name !== null && q.name !== '' && q.name !== undefined) {
+            res.end("Hello " + q.name);
+        } else {
+            res.end("Hello from HieuDD!");
+        }
+    });
+server.listen(3000);
